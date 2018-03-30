5439d02f92ea1fb06e30c9510da9d77c9e65a6ac
Alex <alexandre.charlot@wisembly.com>
2018-03-31 01:12:20 +0200

    pushing prod

---
+++ webpack.prod.config.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg|ttf|eot|svg|woff)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+            }  
+          }
+        ]
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          query: { compact: true }
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
+}
